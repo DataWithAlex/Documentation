d603bf296b929836a0b8c269cb89b1b7e54cae7a
DataWithAlex <explore@datawithalex.com>
2024-02-22 12:40:03 -0500

    update equations

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -1,12 +1,10 @@
 // @ts-check
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import lightCodeTheme from 'prism-react-renderer/themes/github';
+import darkCodeTheme from 'prism-react-renderer/themes/dracula';
+import math from 'remark-math';
+import katex from 'rehype-katex';
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
-
-
-/** @type {import('@docusaurus/types').Config} */
+/** @type {import('@docusaurus/types').Config}*/
 const config = {
   title: 'Explore Data with Alex',
   tagline: 'View the Documentation for Data Science Skills',
@@ -21,34 +19,27 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      '@docusaurus/preset-classic',
+      {
         docs: {
-          // id: 'docs', // omitted => default instance
           routeBasePath: 'docs',
           sidebarPath: require.resolve('./sidebars.js'),
           remarkPlugins: [math],
           rehypePlugins: [katex],
-          // Please change this to your repo.
           editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
-
   plugins: [
     [
       '@docusaurus/plugin-content-docs',
@@ -57,7 +48,6 @@ const config = {
         path: 'statistics',
         routeBasePath: 'statistics',
         sidebarPath: require.resolve('./sidebarsStatistics.js'),
-        // ... other options
       },
     ],
     [
@@ -67,105 +57,100 @@ const config = {
         path: 'azure',
         routeBasePath: 'azure',
         sidebarPath: require.resolve('./sidebarsAzure.js'),
-        // ... other options
       },
     ],
   ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      colorMode: {
-        // "dark" | "light"
-        defaultMode: 'dark' },
-      navbar: {
-        title: 'DataWithAlex',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/alex.png',
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+    },
+    navbar: {
+      title: 'DataWithAlex',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/alex.png',
+      },
+      items: [
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'intro',
+          label: 'Docs',
         },
-        items: [
-          {
-            type: 'doc',
-            position: 'left',
-            docId: 'intro',
-            label: 'Docs',
-          },
-          {
-            to: '/statistics/intro',   // To highlight the navbar item, you must link to a document, not a top-level directory
-            position: 'left',
-            label: 'Statistics',
-            activeBaseRegex: `/statistics/`,
-          },
-          {
-            to: '/azure/intro',   // To highlight the navbar item, you must link to a document, not a top-level directory
-            position: 'left',
-            label: 'Azure',
-            activeBaseRegex: `/azure/`,
-          },
-          {
-            to: '/blog',
-            label: 'Blog',
-            position: 'left'
-          },
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-          
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-               label: 'Tutorial',
-               to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        {
+          to: '/statistics/intro',
+          position: 'left',
+          label: 'Statistics',
+          activeBaseRegex: `/statistics/`,
+        },
+        {
+          to: '/azure/intro',
+          position: 'left',
+          label: 'Azure',
+          activeBaseRegex: `/azure/`,
+        },
+        {
+          to: '/blog',
+          label: 'Blog',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
 };
 
-module.exports = config;
+export default config;
